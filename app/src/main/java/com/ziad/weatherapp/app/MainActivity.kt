767c1b6020780dfcd7945fe8681421c0d8c65372
ZiadAbtar <abtar.ziad@gmail.com>
2020-01-28 11:56:27 +0200

    adding splash activity
    +minor re-packaging

--- app/src/main/java/com/ziad/weatherapp/app/MainActivity.kt
+++ app/src/main/java/com/ziad/weatherapp/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ziad.weatherapp
+package com.ziad.weatherapp.app
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.ziad.weatherapp.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +21,8 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_multiple_cities, R.id.navigation_current_location
+                R.id.navigation_multiple_cities,
+                R.id.navigation_current_location
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/com/ziad/weatherapp/app/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.ziad.weatherapp.app
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.ziad.weatherapp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    private val handler = Handler()
+    private var runnable: Runnable = Runnable {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable, 1500)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+
+    }
+}
