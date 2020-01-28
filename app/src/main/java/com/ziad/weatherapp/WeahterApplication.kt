7012cc953b9ccdd229491933a42345bb90d2c0cb
ZiadAbtar <abtar.ziad@gmail.com>
2020-01-28 13:21:29 +0200

    adding singleton to application

--- app/src/main/java/com/ziad/weatherapp/WeahterApplication.kt
+++ app/src/main/java/com/ziad/weatherapp/WeahterApplication.kt
@@ -1,7 +1,23 @@
 package com.ziad.weatherapp
 
 import android.app.Application
+import java.lang.RuntimeException
 
 class WeahterApplication : Application() {
 
+    companion object {
+        lateinit var instance: WeahterApplication
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    fun get(): WeahterApplication {
+        if (instance == null) {
+            throw RuntimeException("WeatherApplication instance not set")
+        }
+        return instance
+    }
 }
