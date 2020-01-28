dd8e990822b57664fa26e206bd2b852e7b153c41
ZiadAbtar <abtar.ziad@gmail.com>
2020-01-28 15:51:35 +0200

    adding retrofit initial implementation

---
+++ app/src/main/java/com/ziad/weatherapp/data/remote/APIServer.kt
@@ -0,0 +1,29 @@
+package com.ziad.weatherapp.data.remote
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object APIServer {
+
+    @JvmStatic
+    val BASE_URL = "https://samples.openweathermap.org/data/2.5/"
+
+    private fun buildRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(OkHttpClient())
+            .build()
+    }
+
+    private var retrofit = buildRetrofit()
+
+    private var _apiServer = retrofit.create(APIInterface::class.java)
+
+    @JvmStatic
+    val apiServer: APIInterface
+        get() = _apiServer
+
+}
